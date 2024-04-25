6d193540ec80b9787513662166ce84d6917887cb
janaelshafie <168121874+janaelshafie@users.noreply.github.com>
2024-04-26 02:29:45 +0300

    Update Login_SignUp.java

--- components/Login_SignUp.java
+++ components/Login_SignUp.java
@@ -1,25 +1,19 @@
-
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.HashMap;
 import java.util.Scanner;
-
 public class Login_SignUp {
-    private String username;
-    private String password;
-    static HashMap <String,String> userInfo = new HashMap<String,String>();
+    private final String username;
+    private final String password;
+    static HashMap <String,String> userInfo = new HashMap<>();
     public Login_SignUp (String username, String password){
         this.username = username;
         this.password = password;
     }
 
-    public String getUsername(){
-        return username;
-    }
+
 public static void SignUp(Scanner in){
     System.out.println("Please enter your first name: ");
     String first = in.nextLine();
@@ -54,12 +48,22 @@ public static void SignUp(Scanner in){
     }
     System.out.println("Enter your password: ");
     String password = in.nextLine();
-    userInfo.put(username,password);
-    file(username,password);
+    try{
+        writefile(username,password);
+    }
+    catch(IOException e){
+        System.out.println("Error writing into file." + e.getMessage());
+    }
     System.out.println("Sign Up is Successful!");
 }
 
 public static void LogIn(Scanner in){
+        try{
+            readFile();
+        }
+        catch (IOException e){
+            System.out.println("Error reading from file." + e.getMessage());
+        }
     System.out.println("Enter your username: ");
     String username = in.nextLine();
     System.out.println("Enter your password: ");
@@ -76,13 +80,23 @@ public static void LogIn(Scanner in){
     }
 }
 
-public static void file(String username,String password) {
-    try (BufferedWriter writer = new BufferedWriter(new FileWriter("user_registeration_info.txt", true))) {
+public static void writefile(String username,String password) throws IOException {
+    BufferedWriter writer = new BufferedWriter(new FileWriter("D:\\Java\\Advanced CP\\src\\user_info.txt",true));
         writer.write(username + "," + password);
         writer.newLine();
+        writer.flush();
+        writer.close();
         }
-    catch (IOException e){
-        System.out.println("Error saving user information to file: " + e.getMessage());
-         }
+public static void readFile() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("D:\\Java\\Advanced CP\\src\\user_info.txt"));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(",");
+                if (parts.length == 2) {
+                    String username = parts[0];
+                    String password = parts[1];
+                    userInfo.put(username, password);
+                }
+            }
+        }
     }
-}
